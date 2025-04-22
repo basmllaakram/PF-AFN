c713890aba6a4ff6fc06322f291bb3e892257c24
basmllaakram <110834284+basmllaakram@users.noreply.github.com>
2025-04-22 17:07:34 +0200

    Update correlation.py

--- PF-AFN_test/models/correlation/correlation.py
+++ PF-AFN_test/models/correlation/correlation.py
@@ -271,9 +271,12 @@ def cupy_kernel(strFunction, objVariables):
 	return strKernel
 # end
 
-@cupy.util.memoize(for_each_device=True)
+import cupy
+from cupy._util import memoize
+@memoize(for_each_device=True)
 def cupy_launch(strFunction, strKernel):
-	return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
+    return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
+
 # end
 
 class _FunctionCorrelation(torch.autograd.Function):
